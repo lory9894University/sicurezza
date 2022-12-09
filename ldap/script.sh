bbb5d176d931bd0f9a40364538a5422701e4134b
Lorenzo Dentis <lory9894@gmail.com>
2022-12-09 12:08:07 +0100

    ldap

---
+++ ldap/script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -e
+docker run --name ldap-service -p 1389:1389 -p 1636:1636 --hostname ldap-service --detach osixia/openldap:1.1.8
+docker run --name phpldapadmin-service --hostname phpldapadmin-service --link ldap-service:ldap-host --env PHPLDAPADMIN_LDAP_HOSTS=ldap-host --detach osixia/phpldapadmin:0.9.0
+
+PHPLDAP_IP=$(docker inspect -f "{{ .NetworkSettings.IPAddress }}" phpldapadmin-service)
+
+echo "Go to: https://$PHPLDAP_IP"
+echo "Login DN: cn=admin,dc=example,dc=org"
+echo "Password: admin"
